36987d9a9b72eb466214c7ccfd13fe60056e6e8f
delavet <yhang1996@126.com>
2019-05-05 14:07:58 +0800

    first generation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Search from './utils/Search';
+import {getAllJsonp} from "./utils/jsonp";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// var items = {result:['','']}
+var items = null;
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items
+    }
+    this.message = this.message.bind(this);
+    this.query = "items";
+  }
+
+  componentDidMount(){
+      //getAllJsonp("西瓜")//success
+      //getAllAxios("西瓜");//fail
+      this.setState({
+          items:""
+      })
+    }
+
+
+  message (query){
+    this.query = query;
+    console.log(this.query);
+    getAllJsonp(this.query).then((myjson) => {
+        //测试用例是items
+        console.log(myjson.result);
+        this.setState({
+            items:myjson
+        })
+        })
+      // ContactsAPI.getAll(this.query).then((myjson) => {
+      //     console.log(myjson.result);
+      //     this.setState({
+      //         items:myjson
+      //     })
+      // })
+
+  }
+
+  render() {
+    return (
+        <Search onMessage = {this.message} items = {this.state.items} />
+    );
+  }
 }
 
 export default App;
