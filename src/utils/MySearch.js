2e645625688b8e6add0207d1c99cd283c5fccd6c
delavet <yhang1996@126.com>
2019-06-29 20:53:52 +0800

    add link the original post in SO for recomendadtion result

--- src/utils/MySearch.js
+++ src/utils/MySearch.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
-import {Col, Row, Input, Divider, Tooltip} from 'antd';
-const { Search } = Input;
-const divStyle={
-    marginTop:10,
-    marginLeft:'auto',
-    marginRight:'auto',
+import {
+    Col,
+    Row,
+    Input,
+    Divider,
+    Tooltip,
+    Select,
+    PageHeader
+} from 'antd';
+const {Search} = Input;
+const divStyle = {
+    margin: 10
+}
+const {Option} = Select;
+
+function handleChange(value) {
+    console.log(`selected ${value}`);
 }
 
 class MySearch extends Component {
@@ -45,21 +56,26 @@ class MySearch extends Component {
 
     render() {
         let {items} = this.props;
-        if(items) console.log("items:"+items)
+        if (items) 
+            console.log("items:" + items)
         let screen;
         if (items) {
             screen = (
                 <ol className='contact-list'>
                     {
-                        items.data
+                        items
+                            .data
                             .map(
                                 (item, index) => <li key={index} className='contact-list-item'>
                                     <div className='contact-details'>
                                         <p>
-                                            <code><pre>{item.code}</pre></code>
+                                            <code>
+                                                <pre>{item.code}</pre>
+                                            </code>
                                         </p>
+                                        <a href={"https://stackoverflow.com/questions/" + item.id}>reach the original post</a>
                                         <p>{item.id}</p>
-                                        
+
                                     </div>
                                 </li>
                             )
@@ -73,17 +89,19 @@ class MySearch extends Component {
                         padding: "10px"
                     }}
                     className='null'>
-                    <Tooltip title="No to show"><span>No recommanded code to show</span></Tooltip>
+                    <Tooltip title="No to show">
+                        <span>No recommanded code to show</span>
+                    </Tooltip>
                 </div>
             )
         }
 
         return (
-            <div className = "list-contacts">
+            <div className="list-contacts">
                 <Row >
                     <Col span={24}>
-                        <Search 
-                            placeholder="Input  Your Question Here" 
+                        <Search
+                            placeholder="Input  Your Question Here"
                             onSearch={this.searchButton}
                             onKeyPress={(event) => {
                                 this.handleEnter(event)
@@ -91,20 +109,33 @@ class MySearch extends Component {
                             onChange={(event) => {
                                 this.updateQuery(event.target.value, this.state.tag)
                             }}
-                            enterButton />
+                            enterButton/>
                     </Col>
                 </Row>
                 <Row>
-                    <Col span={8} offset={8} >
+                    <Col span={8}>
+                        <Select
+                            placeholder="language"
+                            style={{margin:10, width:100}}
+                            onChange={handleChange}>
+                            <Option value="SQL">SQL</Option>
+                            <Option value="Python">Python</Option>
+                        </Select>
+                    </Col>
+                    <Col span={8}>
                         <div style={divStyle}>
                             <Input
-                                onKeyPress={(event) => {this.handleEnter(event)}}
+                                onKeyPress={(event) => {
+                                    this.handleEnter(event)
+                                }}
                                 placeholder='Tag Here'
-                                onChange={(event) => {this.updateQuery(this.state.query, event.target.value)}}/>
+                                onChange={(event) => {
+                                    this.updateQuery(this.state.query, event.target.value)
+                                }}/>
                         </div>
                     </Col>
                 </Row>
-                <Divider />
+                <Divider style={{margin:10}}/>
                 <Row>{screen}</Row>
 
             </div>
